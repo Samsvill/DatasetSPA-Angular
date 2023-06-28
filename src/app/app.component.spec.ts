fb08145960ff20ae38d05fbb9bdb5aa3a21d68ed
Samsvill <samantha.sharid@gmail.com>
2023-06-28 14:38:23 -0500

    re-done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clienteIris'`, () => {
+  it(`should have as title 'proyectoDatasetSPA-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clienteIris');
+    expect(app.title).toEqual('proyectoDatasetSPA-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clienteIris app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proyectoDatasetSPA-Angular app is running!');
   });
 });
